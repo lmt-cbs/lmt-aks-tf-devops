652c5a1744520915631d6f7d2ff2ac2058bf180f
lmtbelmonte <lmtbelmonte@gmail.com>
2023-12-26 08:40:00 +0100

    Changes to storage account files for deploy of terraform state correctly

--- storage-account/az-remote-backend-main.tf
+++ storage-account/az-remote-backend-main.tf
@@ -12,39 +12,39 @@ resource "random_string" "tf-name" {
 
 # Create a Resource Group for the Terraform State File
 resource "azurerm_resource_group" "state-rg" {
-  name     = "${lower(var.company)}-tfstate-rg"
+  name     = "${var.deploy}-terraform-state"
   location = var.location
 
- # lifecycle {
- #   prevent_destroy = true
- # }
+  lifecycle {
+    prevent_destroy = true
+  }
   tags = {
-    environment = var.environment
+    environment = var.env
   }
 }
 
 # Create a Storage Account for the Terraform State File
 resource "azurerm_storage_account" "state-sta" {
   depends_on                = [azurerm_resource_group.state-rg]
-  name                      = "${lower(var.company)}tf${random_string.tf-name.result}"
+  name                      = "${lower(var.deploy)}tf${random_string.tf-name.result}"
   resource_group_name       = azurerm_resource_group.state-rg.name
   location                  = azurerm_resource_group.state-rg.location
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  access_tier               = "Hot"
-  account_replication_type  = "ZRS"
-  enable_https_traffic_only = true
+  account_kind              = var.account_kind
+  account_tier              = var.account_tier
+  access_tier               = var.access_tier
+  account_replication_type  = var.account_replication_type
+  enable_https_traffic_only = var.enable_https_traffic_only
 
-  lifecycle {
-    prevent_destroy = true
-  }
+ lifecycle {
+   prevent_destroy = true
+ }
   tags = {
-    environment = var.environment
+    environment = var.env
   }
 }
 # Create a Storage Container for the Core State File
 resource "azurerm_storage_container" "core-container" {
   depends_on           = [azurerm_storage_account.state-sta]
-  name                 = "${lower(var.company)}-core-tfstate"
+  name                 = "${lower(var.deploy)}-core-state"
   storage_account_name = azurerm_storage_account.state-sta.name
 }
